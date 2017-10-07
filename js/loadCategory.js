a1c1509f6a70c78a7d91f67acdc50b3c5a630246
Ayellet Tal <tal.ayellet@gmail.com>
2017-10-08 00:42:52 +0300

    Fixing category css to remove template + Making the category items load generic. TBD - Minor css fixes & adding pagination logic

---
+++ js/loadCategory.js
@@ -0,0 +1,45 @@
+/**
+ * Created by Ayellet on 27-Sep-17.
+ */
+// set namespace
+var a = {};
+
+// Set constants
+const TAGS_CATEGORY1 = '<a href="category.html">';
+const TAGS_CATEGORY2 = '</a></li>';
+
+const TAGS1 = '<div class="col-lg-3 col-md-4 col-sm-6"><div class="thumbnail"><a href="#"><img class="img-thumbnail" src="';
+const TAGS2 = '" </a><div class="caption"><a href="#"><h3>';
+const TAGS3 = '</a><p class="categoryItem">';
+const TAGS4 = '</p></div></div></div><!-- /. col end-->';
+
+// Set global objects
+a.products = {};
+
+// Set methods
+a.fLoadCategoryName = function (categoryName) {
+    $("#currentCategory").html(TAGS_CATEGORY1 + categoryName + TAGS_CATEGORY2);
+    $("#categoryLabelId").text(categoryName);
+}
+
+a.fLoadCategoryProducts = function (contentObj) {
+    // validate contentObj
+    var count = contentObj.length;
+    if(count === 0) {
+        console.log("invalid input"); //DEBUG
+        return;
+    }
+
+    var output = '';
+    for(var i = 0; i < count; i++){
+        output = TAGS1 + contentObj[i].image + TAGS2 + contentObj[i].label + TAGS3 + contentObj[i].description + TAGS4;
+        $("#rowFirst").append(output);
+    }
+}
+
+a.fGetItems = function (jsonPath, productsObj) {
+    $.getJSON(jsonPath, function (data) {
+        productsObj = data.products;
+        a.fLoadCategoryProducts(productsObj);
+    })
+}
